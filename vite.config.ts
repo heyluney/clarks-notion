0b79b6520e9c78423d4a0b176c3a03c4ef3af5b8
helenyu <rinmuses@gmail.com>
2025-02-19 08:38:07 -0800

    Added coverage to vitest.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
+/// <reference types="vitest" /> # allows reference to vitest types
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/clarks-notion",
+  test: {
+    coverage: {
+      provider: 'v8'
+    }
+  },
   plugins: [react()],
 })
